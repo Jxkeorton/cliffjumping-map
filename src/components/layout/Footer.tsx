d93ab1561a6a67396ba7a6d20a68071edc621692
Jxkeorton <jakeorton99@gmail.com>
2023-07-08 16:12:04 +0100

    changed footer around

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,38 +1,18 @@
 
 
 const Footer = () => {
-  const hoursOfOperation = [
-    'Mon-Thu: 9AM to 8PM',
-    'Friday: 9AM to 5PM',
-    'Sat-Sun: Jumping!'
-  ];
 
-  const contactEmail = 'info@mountainmanbase.com';
+  const contactEmail = 'jakelewisorton@gmail.com';
 
   const menuLinks = [
-    { label: 'Home', url: 'https://www.mountainmanbase.com/' },
-    { label: 'Learn to Base', url: 'https://www.mountainmanbase.com/learn-to-base' },
-    { label: 'Media', url: 'https://www.mountainmanbase.com/media' },
-    { label: 'Events', url: 'https://www.mountainmanbase.com/high-performance' }
+    { label: 'Facebook group', url: 'https://www.facebook.com/groups/ukcliffjumping' },
   ];
 
   return (
     <footer className="footer">
       <div className="contain">
         <div className="col">
-          <h3>UK cliff jump map</h3>
-          <ul>
-            <li>© 2023 by Apex</li>
-          </ul>
-        </div>
-
-        <div className="col">
-          <h1>Hours of operation</h1>
-          <ul>
-            {hoursOfOperation.map((hours, index) => (
-              <li key={index}>{hours}</li>
-            ))}
-          </ul>
+          <h1>UK cliff jumping map</h1>
         </div>
 
         <div className="col">
