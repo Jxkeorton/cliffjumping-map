f5f04c9efc5a4db8911e78b6c88e562ea807378e
Jxkeorton <jakeorton99@gmail.com>
2023-07-22 01:12:40 +0100

    google pin fix, was mixing up longitude and latitude points when being directed to google maps

--- src/components/pages/Location.tsx
+++ src/components/pages/Location.tsx
@@ -172,7 +172,7 @@ function Location() {
     const [latitude, longitude] = location?.geometry.coordinates ?? [];
 
     if (latitude && longitude) {
-      const url = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
+      const url = `https://www.google.com/maps/search/?api=1&query=${longitude},${latitude}`;
       window.open(url, '_blank');
     }
   };
